ac83a6ff0dc9cd0110e4c4757602feadc990162d
Parth Dobariya <dobariyaparth25192@gmail.com>
2020-04-08 16:48:27 +0900

    added UI
    added UI

--- Calculator App/app/src/main/java/app/parth/in/calculatorapp/MainActivity.kt
+++ Calculator App/app/src/main/java/app/parth/in/calculatorapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package app.parth.`in`.calculatorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button Worked", Toast.LENGTH_SHORT).show()
     }
 }
