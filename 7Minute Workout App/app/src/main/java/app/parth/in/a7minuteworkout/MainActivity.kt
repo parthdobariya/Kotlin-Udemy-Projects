9c38b91be671d9bc060b4ce2a18c1fbf564e386c
Parth Dobariya <dobariyaparth25192@gmail.com>
2020-08-16 21:53:28 +0900

    setting up the UI
    setting up the UI

--- 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/MainActivity.kt
+++ 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/MainActivity.kt
@@ -1,11 +1,17 @@
 package app.parth.`in`.a7minuteworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llStart.setOnClickListener {
+            Toast.makeText(this, "Hear We Will Start The Exercise", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
