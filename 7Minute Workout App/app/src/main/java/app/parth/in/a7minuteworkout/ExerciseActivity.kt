95e34406faff958c459941500026851e8a7cc7d1
Parth Dobariya <dobariyaparth25192@gmail.com>
2020-08-17 18:32:05 +0900

    - added in ExerciseActivity Using CountDownTimers To Display The Seconds Remaining
    - added in ExerciseActivity Using CountDownTimers To Display The Seconds Remaining

--- 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/ExerciseActivity.kt
+++ 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/ExerciseActivity.kt
@@ -1,10 +1,16 @@
 package app.parth.`in`.a7minuteworkout
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+    private var restProcess = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
@@ -15,5 +21,43 @@ class ExerciseActivity : AppCompatActivity() {
         toolbar_exercise_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
     }
+
+    override fun onDestroy() {
+        if (restTimer !=  null) {
+            restTimer!!.cancel()
+            restProcess = 0
+        }
+        super.onDestroy()
+
+    }
+
+    private fun setRestProgressBar() {
+        progressBar.progress = restProcess
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProcess++
+                progressBar.progress = 10 - restProcess
+                tvTimer.text = (10 - restProcess).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Hear now we will start the exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProcess = 0
+        }
+        setRestProgressBar()
+    }
+
 }
