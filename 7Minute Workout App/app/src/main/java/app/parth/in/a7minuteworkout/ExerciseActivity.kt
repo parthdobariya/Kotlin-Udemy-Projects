325d7fc2986bae80d77f7fbd9c36d7fdf9789d93
Parth Dobariya <dobariyaparth25192@gmail.com>
2020-09-23 09:45:53 +0900

    - added  The SqliteOpenHelper And Storing Date In The DB
    - added  The SqliteOpenHelper And Storing Date In The DB

--- 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/ExerciseActivity.kt
+++ 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/ExerciseActivity.kt
@@ -19,11 +19,11 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var restTimer: CountDownTimer? = null
     private var restProcess = 0
-    private var restTimerDuration: Long = 10
+    private var restTimerDuration: Long = 1 //10
 
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProcess = 0
-    private var exerciseTimerDuration: Long = 30
+    private var exerciseTimerDuration: Long = 1 //30
 
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
--- 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/FinishActivity.kt
+++ 7Minute Workout App/app/src/main/java/app/parth/in/a7minuteworkout/FinishActivity.kt
@@ -1,8 +1,11 @@
 package app.parth.`in`.a7minuteworkout
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_finish.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,19 @@ class FinishActivity : AppCompatActivity() {
         btnFinish.setOnClickListener {
             finish()
         }
+        addDateToDatabase()
+    }
+
+    private fun addDateToDatabase() {
+        val calendar = Calendar.getInstance()
+        val dateTime = calendar.time
+        Log.i("DATE", "" + dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+
+        val dbHandler = SqliteOpenHelper(this, null)
+        dbHandler.addDate(date)
+        Log.i("DATE:", "Added")
     }
 }
