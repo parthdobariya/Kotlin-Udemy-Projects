f6df7ef75377e2f184f27c6ea8c1bfd0c481533e
Parth Dobariya <dobariyaparth25192@gmail.com>
2020-09-24 00:13:48 +0900

    Added The Add HappyPlaceActivity and using dimens, string, color
    Added The Add HappyPlaceActivity and using dimens, string, color

--- Happy Places App/app/src/main/java/app/parth/in/happyplaces/AddHappyPlaceActivity.kt
+++ Happy Places App/app/src/main/java/app/parth/in/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,17 @@
-package app.parth.`in`.happyplaces
+ package app.parth.`in`.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+ class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+        setSupportActionBar(toolbar_add_place)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_add_place.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
