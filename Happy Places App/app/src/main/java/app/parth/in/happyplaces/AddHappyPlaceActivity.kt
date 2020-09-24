52ba6b5b8f6c3a90a765af8c26c78f91efc15363
Parth Dobariya <dobariyaparth25192@gmail.com>
2020-09-24 16:01:32 +0900

    Added Calendar Picker And Populate Our Date Edit Text
    Added Calendar Picker And
    Populate Our Date Edit Text

--- Happy Places App/app/src/main/java/app/parth/in/happyplaces/AddHappyPlaceActivity.kt
+++ Happy Places App/app/src/main/java/app/parth/in/happyplaces/AddHappyPlaceActivity.kt
@@ -1,10 +1,18 @@
- package app.parth.`in`.happyplaces
+package app.parth.`in`.happyplaces
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.text.SimpleDateFormat
+import java.util.*
 
- class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -13,5 +21,33 @@ import kotlinx.android.synthetic.main.activity_add_happy_place.*
         toolbar_add_place.setNavigationOnClickListener {
             onBackPressed()
         }
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        et_date.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        et_date.setText(sdf.format(cal.time).toString())
     }
 }
